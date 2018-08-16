b1593826d077222af8bad18fe2371105aec2ac25
mikoto2000 <mikoto2000@gmail.com>
2018-08-16 22:15:04 +0900

    '3 でも 5 でも割れない場合は数値そのまま' のテストを追加。

--- test/test_fizzbuzz.cpp
+++ test/test_fizzbuzz.cpp
@@ -23,5 +23,11 @@ TEST(fizzbuzz_get_answer, 3_and_5) {
     ASSERT_STREQ("FizzBuzz", fizzbuzz_get_answer(30));
 }
 
+TEST(fizzbuzz_get_answer, 1_and_2_and_4) {
+    ASSERT_STREQ("1", fizzbuzz_get_answer(1));
+    ASSERT_STREQ("2", fizzbuzz_get_answer(2));
+    ASSERT_STREQ("4", fizzbuzz_get_answer(4));
+}
+
 }
 
