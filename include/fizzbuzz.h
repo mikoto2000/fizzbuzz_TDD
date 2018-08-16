3dc7ca169c3a97e7ad6e562537d333b3e9339617
mikoto2000 <mikoto2000@gmail.com>
2018-08-16 22:43:32 +0900

    '3 でも 5 でも割れない場合は数値そのまま' の実装を追加。

--- include/fizzbuzz.h
+++ include/fizzbuzz.h
@@ -5,6 +5,9 @@ typedef unsigned char fizzbuzz_number_t;
  * @fn fizzbuzz ルールに従い、番号に対応する回答を返却する。
  * @param (number) 回答が欲しい番号
  * @return         番号に対応する回答の文字列
+ *
+ * @description この関数で取得した文字列は、 fizzbuzz_free_answer() で開放してください。
+ *
  */
 char* fizzbuzz_get_answer(fizzbuzz_number_t number);
 
--- src/fizzbuzz.c
+++ src/fizzbuzz.c
@@ -1,4 +1,10 @@
 #include "fizzbuzz.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// fizzbuzz_number_t が unsigned char なので、
+// 最大 3 桁 + '\0' で 4 に設定。
+#define MAX_DIGIT 4
 
 char* fizzbuzz_get_answer(fizzbuzz_number_t number) {
     if (number % 15 == 0) {
@@ -13,6 +19,8 @@ char* fizzbuzz_get_answer(fizzbuzz_number_t number) {
         return "Buzz";
     }
 
-    return "";
+    char* buf = (void*)malloc(sizeof(char) * MAX_DIGIT);
+    snprintf(buf, MAX_DIGIT, "%d", number);
+    return buf;
 }
 
